d2a3c102f41c5e599ca6d5f9c9c1130109cb9d90
nguyenquochung03 <nguyenhungnqh27@gmail.com>
2025-02-17 17:43:08 +0700

    update client

--- client/vite.config.js
+++ client/vite.config.js
@@ -4,4 +4,13 @@ import react from "@vitejs/plugin-react";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    minify: false, // Không nén để tiết kiệm RAM
+    chunkSizeWarningLimit: 1000, // Tăng giới hạn file
+    rollupOptions: {
+      output: {
+        manualChunks: undefined, // Không chia nhỏ file quá mức
+      },
+    },
+  },
 });
